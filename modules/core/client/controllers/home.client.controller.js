3be8854c3b19031063c517ff0aecbafb8d82bb87
Julian Wise - WLAPTOP <Julianadamwise@gmail.com>
2016-02-21 17:08:29 +1100

    Pre DB of the mean app

--- modules/core/client/controllers/home.client.controller.js
+++ modules/core/client/controllers/home.client.controller.js
@@ -4,5 +4,47 @@ angular.module('core').controller('HomeController', ['$scope', 'Authentication',
   function ($scope, Authentication) {
     // This provides Authentication context.
     $scope.authentication = Authentication;
+
+    $scope.alerts = [
+      {
+        icon: 'glyphicon-user',
+        colour:'btn-success',
+        total:'20,408',
+        description: 'TOTAL CUSTOMERS'
+      },
+      {
+        icon: 'glyphicon-calendar',
+        colour:'btn-primary',
+        total:'8,382',
+        description: 'UPCOMING EVENTS'
+      },
+      {
+        icon: 'glyphicon-edit',
+        colour:'btn-success',
+        total:'527',
+        description: 'NEW CUSTOMERS IN 24H'
+      },
+      {
+        icon: 'glyphicon-record',
+        colour:'btn-info',
+        total:'80,762',
+        description: 'EMAILS SENT'
+      },
+      {
+        icon: 'glyphicon-eye-open',
+        colour:'btn-warning',
+        total:'8,382',
+        description: 'FOLLOWUPS REQUIRED'
+      },
+      {
+        icon: 'glyphicon-flag',
+        colour:'btn-danger',
+        total:'348',
+        description: 'REFERRALS TO MODERATE'
+      }
+
+    ];
+
+
   }
 ]);
